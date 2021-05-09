10b36bf77eb9dca1438ac9a712f0abd91c0a0997
eugbyte <eugenetham1994@gmail.com>
2021-05-10 00:12:53 +0800

    chore: added select after pasting

--- LegalCitationVSTO/ThisAddIn.cs
+++ LegalCitationVSTO/ThisAddIn.cs
@@ -8,6 +8,7 @@ using Office = Microsoft.Office.Core;
 using Tools = Microsoft.Office.Tools.Word;
 using Microsoft.Office.Interop.Word;
 using System.Windows.Forms;
+using System.Text.RegularExpressions;
 
 namespace LegalCitationVSTO
 {
@@ -29,18 +30,36 @@ namespace LegalCitationVSTO
             vstoDoc.SelectionChange += new Tools.SelectionEventHandler(ThisDocument_SelectionChange);
         }
 
-        void ThisDocument_SelectionChange(object sender, Tools.SelectionEventArgs e)
+        private void ThisDocument_SelectionChange(object sender, Tools.SelectionEventArgs e)
         {
-            Document document = e.Selection.Document;
+            Document document = e.Selection.Document;            
             Range previous = e.Selection.Previous();
             if (previous == null || previous.Paragraphs == null) return;
+            int numParas = previous.Paragraphs.Count;
+            int count = previous.Footnotes.Count;
+            if (count != 0) return; 
 
             Paragraph paragraph = previous.Paragraphs[1];
-
             string text = paragraph.Range.Text;
 
-            MessageBox.Show($"previous: {text}");
+            string pattern = @"__FOOTNOTE__.+__/FOOTNOTE__";
+            MatchCollection mc = Regex.Matches(text, pattern);
+            if (mc.Count != 1) return;
 
+            Match match = mc[0];
+            string footnoteText = match.Value;
+            footnoteText = Regex.Replace(footnoteText, "__FOOTNOTE__", "");
+            footnoteText = Regex.Replace(footnoteText, "__/FOOTNOTE__", "");
+            previous.Footnotes.Add(Range: paragraph.Range, Text: footnoteText);
+
+            previous.Next().Next().Select();
+
+            // e.Selection.Range.InsertParagraphAfter();
+            // e.Selection.Next().Select();
+            
+            return;
+
+            // MessageBox.Show($"previous: {text}");
         }
 
         #region VSTO generated code
