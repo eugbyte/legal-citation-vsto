7830ef1d8944f0efbf834e70354fc0f5396ab09c
eugbyte <eugenetham1994@gmail.com>
2021-05-30 10:59:04 +0800

    chore: added mock fetching of title

--- LegalCitationVSTO/GlobalState.cs
+++ LegalCitationVSTO/GlobalState.cs
@@ -1,8 +1,12 @@
-﻿namespace LegalCitationVSTO
+﻿using System.Collections.Generic;
+
+namespace LegalCitationVSTO
 {
     // To communicate between the ribbon and ThisAddIn
     internal static class GlobalState
     {
         public static bool Enabled { get; set; } = true;
+
+        public static Dictionary<int, string> TitleDict { get; } = new Dictionary<int, string>();
     }
 }
--- LegalCitationVSTO/Word/ClipboardChange.cs
+++ LegalCitationVSTO/Word/ClipboardChange.cs
@@ -4,12 +4,15 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Microsoft.Office.Interop.Word;
 using Microsoft.Office.Tools.Ribbon;
+using RestSharp;
 using WK.Libraries.SharpClipboardNS;
 using static WK.Libraries.SharpClipboardNS.SharpClipboard;
+using Task = System.Threading.Tasks.Task;
 
 namespace LegalCitationVSTO
 {
@@ -38,18 +41,50 @@ namespace LegalCitationVSTO
             string copiedText = this.clipboard.ClipboardText;
             string applicationName = e.SourceApplication.Name;
             string title = e.SourceApplication.Title;
-            int id = e.SourceApplication.ID;
+            int id = title.GetHashCode();
 
-            Debug.WriteLine(copiedText);
             Debug.WriteLine(id);
             Debug.WriteLine(applicationName);
             Debug.WriteLine(title);
+            Debug.WriteLine(copiedText);
 
             if (!title.Contains(".pdf")) return;
 
-            Document doc = this.Application.ActiveDocument;
             RibbonButton button = Globals.Ribbons.Ribbon1.pdfButton;
-            button.Label = title;
+            Dictionary<int, string> titleDict = GlobalState.TitleDict;
+
+            if (!titleDict.ContainsKey(id))
+            {
+                string newTitle = Regex.Replace(title, @".pdf.+", string.Empty);
+                button.Label = newTitle;
+
+                // Note that this is async
+                // So will be completed after code block below
+                Task.Run(async () =>
+                {
+                    string fetchedTitle = await this.GetFullTitle();
+                    newTitle = $"{fetchedTitle}\n{newTitle}";
+                    button.Label = newTitle;
+
+                    Debug.WriteLine(newTitle);
+                    titleDict.Add(id, newTitle);
+                });
+            } else
+            {
+                button.Label = titleDict[id];
+            }
+
+        }
+
+        private async Task<string> GetFullTitle()
+        {
+            Debug.WriteLine("starting fetch...");
+            RestClient client = new RestClient("https://jsonplaceholder.typicode.com");
+            RestRequest request = new RestRequest("todos/1", DataFormat.Json);
+            Dictionary<string, string> result = await client.GetAsync<Dictionary<string, string>>(request);
+            string title = result["title"];
+            Debug.WriteLine($"result: ${title}");
+            return $"{title}";
         }
     }
 }
